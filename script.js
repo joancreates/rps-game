215e6adf538769be4caa5a01efa9d9e832386491
joancreates <135924201+joancreates@users.noreply.github.com>
2023-07-16 17:30:41 -0700

    Create index.html, script.js & write pseudocode
    Write the boilerplate for the html file and write the logic
    for the rock paper and scissors program.

---
+++ script.js
@@ -0,0 +1,24 @@
+//The game is going to have 5 rounds.
+
+//Each time a player wins. The round ends and new round begins.
+
+//When the rounds count equals 5 the game ends and the player with more rounds won wins the game.
+
+//Each round is going to have the following sequence:
+
+//Get input from 2 players( User / Machine )
+
+//There's 3 inputs allowed "Rock", "Paper", and "Scissors"
+
+//There's 3 possible initial scenarios:
+
+//1. If the user inputs Rock and the Machine inputs Scissors.
+//   The user wins and the Machine loses.
+
+//2. If the user inputs Scissors and the Machine inputs Paper.
+//   The user wins and the machine loses.
+
+//3. If the user inputs Paper and the Machine inputs Rock.
+//   The user wins and the machine loses.
+
+//- If the opposite happens on each scenario. The user loses and the Machine wins.
