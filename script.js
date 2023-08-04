53adc099805bd39a8e1e9606fa4b2bea836e2482
joancreates <135924201+joancreates@users.noreply.github.com>
2023-08-03 22:23:41 -0700

    Refactor code functionality, add UI

--- script.js
+++ script.js
@@ -27,76 +27,198 @@
 //Note: Implement recursive function with return function(); 
 //https://stackoverflow.com/questions/59105774/why-is-javascript-recursive-function-returning-undefined-even-when-i-am-return
 
-const btns = document.querySelectorAll("button");
 
-btns.forEach((btn) => btn.addEventListener("click", playGame));
+game();
 
-function playGame() {
+function game(roundResult) {
+
+    const btns = document.querySelectorAll("button[data-select]");
+    btns.forEach((btn) => {btn.addEventListener("click", playGame)});
+
     let playerScore = 0;
     let machineScore = 0;
     let drawsPerRound = 0;
+    let totalScore = 0;
+    let totalWins = 0;
+    let totalGames = 0;
 
-    console.log(this);
+    const roundMsg = document.querySelector("#match-result"); 
+    const yourScore = document.querySelector("#your-score");
+    const rivalScore = document.querySelector("#rival-score");
+    const draws = document.querySelector("#draws");
+    const roundsTracker = document.querySelectorAll("span[data-rounds='tracker'");
+    const buttons = document.querySelector("#buttons");
+    const gamesWon = document.querySelector("#games-won");
+    const gamesPlayed = document.querySelector("#games-played");
 
-    // for (i = 0; i < 5; i++) {  --- Temporarly disable 5 turns logic
-    roundResult = playRound.call
-        (this, getPlayerChoice.call(this), getMachineChoice());
-        
-        alert(roundResult);
+    function disableBtns() {
+        btns.forEach((btn) => btn.disabled = true);
+    };
+    function enableBtns() { 
+        btns.forEach((btn) => btn.disabled = false);
+    };
 
-        if( roundResult.includes("won") ) {
-            playerScore = ++playerScore;
+    function playGame() {
+        roundResult = playRound.call(this, getPlayerChoice.call(this), getMachineChoice());
 
-            alert(`Your Score: ${playerScore}/5\n`+ 
-            `Rival Score: ${machineScore}/5\n`+ 
-            `Draws: ${drawsPerRound}`);
+        function getResultBoard() {
+            roundsTracker.textContent = totalScore;
+            roundMsg.textContent = roundResult;
 
-        } else if( roundResult.includes("lost") ) {
-            machineScore = ++machineScore;
-
-            alert(`Your Score: ${playerScore}/5\n`+
-            `Rival Score: ${machineScore}/5\n`+
-            `Draws: ${drawsPerRound}`);
-
-        } else {
-            // --i; -- Temporary disable 5 turns logic
-            drawsPerRound = ++drawsPerRound;
-
-            alert(`Your Score: ${playerScore}/5\n`+
-            `Rival Score: ${machineScore}/5\n`+
-            `Draws: ${drawsPerRound}`); 
+            yourScore.textContent = 
+            `Your Score: ${playerScore}/${roundsTracker.textContent}`; 
+            rivalScore.textContent = 
+            `Rival Score: ${machineScore}/${roundsTracker.textContent}`;
+            draws.textContent =
+            `Draws: ${drawsPerRound}`;
         }
 
-    // } -- Temporary disable 5 turns logic
+        function getResetBtn() {
+            if(totalScore === 1) {
+            const resetBtn = document.createElement("button");
+            resetBtn.setAttribute("id", "reset-btn")
+            resetBtn.textContent = "Reset";
+            buttons.appendChild(resetBtn);
 
-    if (playerScore > machineScore) {
-        alert("You've won the match!");
-    } else {
-        alert("You've lost the match!");
+            resetBtn.addEventListener("click", resetGame);
+            }
+        }
+        function playAgainBtn() {
+            const playAgainBtn = document.createElement("button");
+            playAgainBtn.setAttribute("id", "play-again-btn")     
+            playAgainBtn.textContent = "Play Again";
+            buttons.appendChild(playAgainBtn);
+
+            playAgainBtn.addEventListener("click", playAgain);
+        }
+
+        function playAgain() {
+            enableBtns();
+            btns.disabled = false;    
+            roundMsg.textContent = "";
+            yourScore.textContent = "";
+            rivalScore.textContent = "";
+            draws.textContent = "";
+            roundsTracker.textContent = "";
+
+            playerScore = 0;
+            machineScore = 0;
+            drawsPerRound = 0;
+            totalScore = 0;
+
+            const resetBtn = document.querySelector("#reset-btn");
+            buttons.removeChild(resetBtn);
+
+            const playAgainBtn = document.querySelector("#play-again-btn");
+            buttons.removeChild(playAgainBtn);
+        }
+        function resetGame() {
+            enableBtns();
+            roundMsg.textContent = "";
+            yourScore.textContent = "";
+            rivalScore.textContent = "";
+            draws.textContent = "";
+            roundsTracker.textContent = "";
+            gamesWon.textContent = "";
+            gamesPlayed.textContent = "";
+
+            playerScore = 0;
+            machineScore = 0;
+            drawsPerRound = 0;
+            totalScore = 0;
+            totalWins = 0;
+            totalGames = 0;
+            
+            const resetBtn = document.querySelector("#reset-btn");
+            buttons.removeChild(resetBtn);
+
+            const playAgainBtn = document.querySelector("#play-again-btn");
+            buttons.removeChild(playAgainBtn);
+        }
+
+        function finishRoundWin() {
+            ++totalWins;
+            ++totalGames;
+            roundMsg.textContent = "You've won the match!";
+            gamesWon.textContent = `Games won: ${totalWins}`;
+            gamesPlayed.textContent = `Games played: ${totalGames}`;
+
+            disableBtns();
+            playAgainBtn();
+        }
+
+        function finishRoundLose() {
+            ++totalGames;
+            roundMsg.textContent = "You've won the match!";
+            gamesWon.textContent = `Games won: ${totalWins}`;
+            gamesPlayed.textContent = `Games played: ${totalGames}`;
+
+            disableBtns();
+            playAgainBtn();
+        }
+
+        if(roundResult.includes("won")) {
+            playerScore = ++playerScore;
+            totalScore = ++totalScore;
+            getResultBoard();
+            getResetBtn();
+        } else if(roundResult.includes("lost")) {
+            machineScore = ++machineScore;
+            totalScore = ++totalScore;
+            getResultBoard();
+            getResetBtn();
+        } else {
+            drawsPerRound = ++drawsPerRound;
+            totalScore = ++totalScore;
+            getResultBoard();
+            getResetBtn();
+        }
+
+        if(playerScore === 5 || machineScore === 5) {
+            if (playerScore > machineScore) {
+            finishRoundWin();
+            } else {
+            finishRoundLose();
+            }
+        }
     }
 }
 
 function playRound(playerSelection, machineSelection) {
+    const winMsg = [
+        "Your rock has flawlessly crushed your opponent's scissors. You've won!",
+        "Your scissors have cut his paper with ease and style. You've won!",
+        "Your paper has fully covered his rock rendering it moveless. You've won!"
+        ];
+    const loseMsg = [
+         "Your scissors have dwindled to the solid power of the rock. You've lost.",
+         "Your paper has been sliced in half by his scissors. You've lost.",
+         "Your rock has been blindfolded by your opponent's paper. You've lost."
+        ];
+    const drawMsg = [
+        "Two rocks have clashed against each other leaving behind crumbles of despair. Shocking, it's a draw!",
+        "Two scissors have collided in an incredible fashion. Such a beautiful and elegant draw!",
+        "Two papers covered each other shrinking into a tiny ball. This looks like a writer's paper bin, it's a draw!"
+        ];
 
-    if(playerSelection === "rock" && machineSelection === "scissors") {
-        return "Your rock has flawlessly crushed your opponent's scissors. You've won!";
-    } else if(playerSelection === "scissors" && machineSelection === "rock") {
-        return "Your scissors have dwindled to the solid power of the rock. You've lost.";
-    } else if(playerSelection === "rock" && machineSelection === "rock") {
-        return "Draw";
-    } else if(playerSelection === "scissors" && machineSelection === "paper") {
-        return "Your scissors have cut his paper with ease and style. You've won!";
-    } else if(playerSelection === "paper" && machineSelection === "scissors") {
-        return "Your paper has been sliced in half by his scissors. You've lost.";
-    } else if(playerSelection === "scissors" && machineSelection === "scissors") {
-        return "Draw";
-    } else if(playerSelection === "paper" && machineSelection === "rock") {
-        return "Your paper has fully covered his rock rendering it moveless. You've won!";
-    } else if(playerSelection === "rock" && machineSelection === "paper") {
-        return "Your rock has been blindfolded by your opponent's paper. You've lost.";
-    } else if(playerSelection === "paper" && machineSelection === "paper") {
-        return "Draw";
-    }
+    if(playerSelection === "rock" && machineSelection === "scissors")
+        return winMsg[0];
+    if(playerSelection === "scissors" && machineSelection === "rock")
+        return loseMsg[0];
+    if(playerSelection === "rock" && machineSelection === "rock")
+        return drawMsg[0];
+    if(playerSelection === "scissors" && machineSelection === "paper") 
+        return winMsg[1];
+    if(playerSelection === "paper" && machineSelection === "scissors")
+        return loseMsg[1];
+    if(playerSelection === "scissors" && machineSelection === "scissors") 
+        return drawMsg[1];
+    if(playerSelection === "paper" && machineSelection === "rock")
+        return winMsg[2];
+    if(playerSelection === "rock" && machineSelection === "paper")
+        return loseMsg[2];
+    if(playerSelection === "paper" && machineSelection === "paper")
+        return drawMsg[2];
 }
 
 function getPlayerChoice(selection) {
